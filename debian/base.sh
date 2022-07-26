e46ce42f910cfc86f1e86973edc6802c19e3f807
L3DLP <web@l3dlp.com>
2022-07-26 22:14:45 +0200

    Add `locate` to install

--- debian/base.sh
+++ debian/base.sh
@@ -5,7 +5,7 @@ sudo curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
 sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y openssl ca-certificates apt-transport-https wget curl dnsutils git gnupg2 htop lsb-release software-properties-common sshfs vim-nox
+sudo apt install -y openssl ca-certificates apt-transport-https wget curl dnsutils git gnupg2 htop locate lsb-release software-properties-common sshfs vim-nox
 
 # SECURITY
 sudo apt install -y ufw
